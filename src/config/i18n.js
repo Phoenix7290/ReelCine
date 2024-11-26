465fcf9d1960c64910f27278b779c8c02ce5e2fa
Phoenix7290 <b5261064@gmail.com>
2024-11-25 21:22:38 -0300

    Locales update and Search Bar

--- src/config/i18n.js
+++ src/config/i18n.js
@@ -1,12 +1,12 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import en from '../locales/en.json';
-import ptBR from '../locales/pt-BR.json';
+import pt from '../locales/pt.json';
 
 i18n.use(initReactI18next).init({
   resources: {
     en: { translation: en },
-    'pt-BR': { translation: ptBR }
+    pt: { translation: pt }
   },
   lng: navigator.language || 'en',
   fallbackLng: 'en',
